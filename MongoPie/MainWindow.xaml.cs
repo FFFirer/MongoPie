2db6c57e21e7c0061c791936f28b64622a715b90
Watt Wang <watt.wang@softtek.com>
2020-01-22 18:07:12 +0800

    Add project files.

---
+++ MongoPie/MainWindow.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MongoPie.ViewModels;
+using MongoDB;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Core;
+
+namespace MongoPie
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainViewModel viewmodel { get; set; }
+
+        #region MongoClient, database, collection
+        private IMongoClient _client { get; set; }
+        public IMongoClient Client
+        {
+            get
+            {
+                return _client;
+            }
+            set
+            {
+                if (_client == value) return;
+                _client = value;
+            }
+        }
+        #endregion
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            viewmodel = new MainViewModel() { 
+                Port = 27017,
+                ServerAddress = "115.159.223.241",
+                UserName= "AdminUser",
+                Pwd = "1qaz@WSX3edc",
+                DbName = "admin",
+                Dbs = new List<MainViewModel.NodeInfo>()
+            };
+            this.Loaded += (s, e) =>
+            {
+                this.DataContext = viewmodel;
+                spStatus.Visibility = Visibility.Hidden;
+                spLogin.Visibility = Visibility.Visible;
+                BindProperty();
+            };
+        }
+
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(viewmodel.ServerAddress))
+            {
+                MessageBox.Show("请填写服务器的地址");
+                return;
+            }
+
+            if ((string.IsNullOrEmpty(viewmodel.UserName) || string.IsNullOrEmpty(viewmodel.Pwd) || string.IsNullOrEmpty(viewmodel.DbName)) && !(string.IsNullOrEmpty(viewmodel.UserName) && string.IsNullOrEmpty(viewmodel.Pwd) && string.IsNullOrEmpty(viewmodel.DbName)))
+            {
+                MessageBox.Show("用户名，密码，数据库名称，全部填写或全部不填写");
+                return;
+            }
+
+            MongoCredential credential = MongoCredential.CreateCredential(viewmodel.DbName, viewmodel.UserName, viewmodel.Pwd);
+            MongoClientSettings setting = new MongoClientSettings()
+            {
+                Credential = credential,
+                Server = new MongoServerAddress(viewmodel.ServerAddress, viewmodel.Port)
+            };
+
+            Client = new MongoClient(setting);
+
+            this.spLogin.Visibility = Visibility.Hidden;
+            this.spStatus.Visibility = Visibility.Visible;
+
+            GetDbInfo();
+        }
+
+        /// <summary>
+        /// 登出
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLogOut_Click(object sender, RoutedEventArgs e)
+        {
+            this.spStatus.Visibility = Visibility.Hidden;
+            this.spLogin.Visibility = Visibility.Visible;
+        }
+
+        /// <summary>
+        /// 绑定
+        /// </summary>
+        private void BindProperty()
+        {
+            this.txtServerAddress.SetBinding(TextBox.TextProperty, new Binding("ServerAddress") { Source = viewmodel });
+
+            this.txtServerPort.SetBinding(TextBox.TextProperty, new Binding("Port") { Source = viewmodel });
+
+            this.txtDbName.SetBinding(TextBox.TextProperty, new Binding("DbName") { Source = viewmodel });
+
+            this.txtUserName.SetBinding(TextBox.TextProperty, new Binding("UserName") { Source = viewmodel });
+
+            this.txtPasswd.SetBinding(PasswordBoxHelper.PasswordProperty, new Binding("Pwd") { Source = viewmodel, Mode = BindingMode.TwoWay, UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged});
+        }
+
+        private void GetDbInfo()
+        {
+            if(Client == null)
+            {
+                throw new NullReferenceException(nameof(Client));
+            }
+
+            // 获取所有database
+            using(var cursor = Client.ListDatabaseNames())
+            {
+                List<string> databasenames = cursor.ToList();
+                int count = databasenames.Count();
+                viewmodel.Dbs.Add(new MainViewModel.NodeInfo()
+                {
+                    Id = 0,
+                    NodeName = "databases",
+                    ParentId = -1,
+                    Nodes = new List<MainViewModel.NodeInfo>(0)
+                });
+                for (int i = 1; i < count; i++)
+                {
+                    viewmodel.Dbs[0].Nodes.Add(new MainViewModel.NodeInfo()
+                    {
+                        Id = i,
+                        NodeName = databasenames[i],
+                        ParentId = 0,
+                        Nodes = new List<MainViewModel.NodeInfo>()
+                    });
+                }
+            }
+
+            this.treeStatus.ItemsSource = viewmodel.Dbs;
+        }
+    }
+}
---
+++ MongoPie/ViewModels/MainViewModel.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MongoPie.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        //public MainViewModel(Type ownerType)
+        //{
+        //    PasswordProperty = DependencyProperty.Register("Password", typeof(string), ownerType, new PropertyMetadata(""));
+        //}
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #region 登录信息
+        private string _serveraddress { get; set; }
+        public string ServerAddress
+        {
+            get
+            {
+                return _serveraddress;
+            }
+            set
+            {
+                if (_serveraddress == value) return;
+                _serveraddress = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private int _port { get; set; }
+        public int Port
+        {
+            get
+            {
+                return _port;
+            }
+            set
+            {
+                if (_port == value) return;
+                _port = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string _dbname { get; set; }
+        public string DbName
+        {
+            get
+            {
+                return _dbname;
+            }
+            set
+            {
+                if (_dbname == value) return;
+                _dbname = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string _username { get; set; }
+        public string UserName
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                if (_username == value) return;
+                _username = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string _pwd { get; set; }
+        public string Pwd
+        {
+            get
+            {
+                return _pwd;
+            }
+            set
+            {
+                if (_pwd == value) return;
+                _pwd = value;
+                NotifyPropertyChanged();
+            }
+        }
+        //public static DependencyProperty PasswordProperty { get; set; }
+        #endregion
+
+        #region 数据库信息
+        private List<NodeInfo> _dbs { get; set; }
+        public List<NodeInfo> Dbs
+        {
+            get
+            {
+                return _dbs;
+            }
+            set
+            {
+                if (_dbs == value) return;
+                _dbs = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public class NodeInfo
+        {
+            public int Id { get; set; }
+            public string NodeName { get; set; }
+            public int ParentId { get; set; }
+            public List<NodeInfo> Nodes { get; set; }
+        }
+        #endregion
+    }
+}
